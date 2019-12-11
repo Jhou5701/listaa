ad18c6025ad32164d354a75f2a40d06e55b0fc79
UsuGIA <jhousaso@gmail.com>
2019-12-10 22:17:43 -0500

    Agregar archivos de proyecto.

---
+++ listaa/ProLista.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace listaa
+{
+    partial class ProLista
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listproducto = new System.Windows.Forms.ListView();
+            this.columncodigo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnnombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnprecio = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnfecha = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnunidad = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtunidad = new System.Windows.Forms.TextBox();
+            this.txtfecha = new System.Windows.Forms.TextBox();
+            this.txtprecio = new System.Windows.Forms.TextBox();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtcodigo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(203, 98);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 25;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 30);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listproducto
+            // 
+            this.listproducto.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columncodigo,
+            this.columnnombre,
+            this.columnprecio,
+            this.columnfecha,
+            this.columnunidad});
+            this.listproducto.HideSelection = false;
+            this.listproducto.Location = new System.Drawing.Point(27, 140);
+            this.listproducto.Name = "listproducto";
+            this.listproducto.Size = new System.Drawing.Size(648, 146);
+            this.listproducto.TabIndex = 23;
+            this.listproducto.UseCompatibleStateImageBehavior = false;
+            this.listproducto.View = System.Windows.Forms.View.Details;
+            // 
+            // columncodigo
+            // 
+            this.columncodigo.Text = "Codigo";
+            this.columncodigo.Width = 96;
+            // 
+            // columnnombre
+            // 
+            this.columnnombre.Text = "Nombre Producto";
+            this.columnnombre.Width = 124;
+            // 
+            // columnprecio
+            // 
+            this.columnprecio.Text = "Precio Venta";
+            this.columnprecio.Width = 97;
+            // 
+            // columnfecha
+            // 
+            this.columnfecha.Text = "Fecha";
+            this.columnfecha.Width = 98;
+            // 
+            // columnunidad
+            // 
+            this.columnunidad.Text = "Unidad";
+            this.columnunidad.Width = 82;
+            // 
+            // txtunidad
+            // 
+            this.txtunidad.Location = new System.Drawing.Point(560, 49);
+            this.txtunidad.Name = "txtunidad";
+            this.txtunidad.Size = new System.Drawing.Size(100, 20);
+            this.txtunidad.TabIndex = 22;
+            this.txtunidad.TextChanged += new System.EventHandler(this.txtunidad_TextChanged);
+            // 
+            // txtfecha
+            // 
+            this.txtfecha.Location = new System.Drawing.Point(438, 49);
+            this.txtfecha.Name = "txtfecha";
+            this.txtfecha.Size = new System.Drawing.Size(100, 20);
+            this.txtfecha.TabIndex = 21;
+            // 
+            // txtprecio
+            // 
+            this.txtprecio.Location = new System.Drawing.Point(295, 49);
+            this.txtprecio.Name = "txtprecio";
+            this.txtprecio.Size = new System.Drawing.Size(100, 20);
+            this.txtprecio.TabIndex = 20;
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(141, 49);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(100, 20);
+            this.txtnombre.TabIndex = 19;
+            // 
+            // txtcodigo
+            // 
+            this.txtcodigo.Location = new System.Drawing.Point(27, 49);
+            this.txtcodigo.Name = "txtcodigo";
+            this.txtcodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtcodigo.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(557, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Unidad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(435, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Fecha Compra";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(292, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Precio Venta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Nombre del producto";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Codigo";
+            // 
+            // ProLista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 293);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listproducto);
+            this.Controls.Add(this.txtunidad);
+            this.Controls.Add(this.txtfecha);
+            this.Controls.Add(this.txtprecio);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.txtcodigo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ProLista";
+            this.Text = "Proyecto Listas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listproducto;
+        private System.Windows.Forms.ColumnHeader columncodigo;
+        private System.Windows.Forms.ColumnHeader columnnombre;
+        private System.Windows.Forms.ColumnHeader columnprecio;
+        private System.Windows.Forms.ColumnHeader columnfecha;
+        private System.Windows.Forms.ColumnHeader columnunidad;
+        private System.Windows.Forms.TextBox txtunidad;
+        private System.Windows.Forms.TextBox txtfecha;
+        private System.Windows.Forms.TextBox txtprecio;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txtcodigo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
